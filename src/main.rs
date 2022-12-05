cc578f064f0c1a6479e60f070535d601adfbd5da
Peichen Guo <guo_peichen@163.com>
2022-12-05 19:01:10 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use crate::memory::memory_hello_world;
 fn main() {
     println!("Hello, world!");
+    memory_hello_world();
 }
---
+++ src/memory/lib.rs
@@ -0,0 +1,5 @@
+pub mod memory{
+    pub fn memory_hello_world(){
+        println!("memory hello world");
+    }
+}
